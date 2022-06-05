7e14247a77ce41c04cdf5729d75725c36cc8a6d1
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-06-05 04:28:22 -0400

    Add support for GIF files

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,20 +10,57 @@ import zipfile
 from pathlib import Path
 
 import streamlit as st
-from PIL import Image
+from PIL import Image, ImageSequence
 from dotenv import load_dotenv
 from gotipy import Gotify
 from loguru import logger
 from rembg.bg import remove  # noqa
 
 
-def remove_bg(bytes_data, path):
-    result = remove(bytes_data)
+def remove_bg(input_data, path):
+    result = remove(input_data)
     img = Image.open(io.BytesIO(result)).convert('RGBA')
-    # img = Image.open(io.BytesIO(bytes_data)).convert('RGBA')  # debug
-    if path.suffix != '.png':
+    if Path(path).suffix != '.png':
         img.LOAD_TRUNCATED_IMAGES = True
     return img
+
+
+def gif2frames(input_file, skip_every=1):
+    im = Image.open(input_file)
+    include_frames = range(0, len(list(ImageSequence.Iterator(im))),
+                           skip_every)
+
+    frames = []
+
+    for n, frame in enumerate(ImageSequence.Iterator(im)):
+        if n not in include_frames:
+            continue
+        frame.copy()
+        bytes_obj = io.BytesIO()
+        frame.save(bytes_obj, format='PNG')
+        frames.append((n, bytes_obj.getvalue()))
+    return frames
+
+
+def frames2gif(frames, duration=100, disposal=2):
+    im_frames = []
+    for frame in frames:
+        im = frame[0]
+        rgba = im.convert('RGBA')
+        data = rgba.getdata()
+        img = [(255, 255, 255, 0) if x[:3] == (0, 0, 0) else x for x in data]
+        rgba.putdata(img)
+        im_frames.append(rgba)
+
+    bytes_obj = io.BytesIO()
+    im_frames[0].save(bytes_obj,
+                      format='GIF',
+                      duration=duration,
+                      disposal=disposal,
+                      loop=0,
+                      save_all=True,
+                      append_images=im_frames[1:])
+    return bytes_obj
 
 
 def main():
@@ -37,18 +74,22 @@ def main():
         st.experimental_rerun()
     st.sidebar.markdown('---')
 
-    accepted_type = ['png', 'jpg', 'jpeg']
-    uploaded_files = st.sidebar.file_uploader('Choose one or multiple files',
-                                              type=accepted_type,
-                                              accept_multiple_files=True,
-                                              key=st.session_state['key'])
+    accept_multiple_files = True
+    accepted_type = ['png', 'jpg', 'jpeg', 'gif']
+
+    uploaded_files = st.sidebar.file_uploader(
+        'Choose one or multiple files',
+        type=accepted_type,
+        accept_multiple_files=accept_multiple_files,
+        key=st.session_state['key'])
 
     if len(uploaded_files) > MAX_FILES != -1:
         st.warning(
             f'Maximum number of files reached! Only the first {MAX_FILES} '
-            'will be processed.'
-        )
+            'will be processed.')
         uploaded_files = uploaded_files[:MAX_FILES]
+
+    uploaded_files = [x for x in uploaded_files if x]
 
     if uploaded_files:
         logger.info(f'Uploaded the following files: {uploaded_files}')
@@ -58,6 +99,20 @@ def main():
         cols = st.empty()
         col1, col2 = cols.columns(2)
         imgs_bytes = []
+        frames = []
+
+        IS_GIF = False
+        if any(Path(x.name).suffix.lower() == '.gif' for x in uploaded_files):
+            IS_GIF = True
+            if len(uploaded_files) > 1:
+                st.error(
+                    f'The maximum number of allowed uploads when processing a '
+                    'GIF is one file!')
+                return
+
+            dur_text = 'Duration (in milliseconds) of each frame:'
+            duration = st.sidebar.slider(dur_text, 0, 1000, 100, 10)
+            frames = gif2frames(uploaded_files[0])
 
         for uploaded_file in uploaded_files:
 
@@ -82,23 +137,39 @@ def main():
 
             pb = progress_bar.progress(0)
 
+            if frames:
+                imgs_bytes = frames
+
             with st.spinner('Wait for it...'):
                 for n, (uploaded_file, bytes_data) in enumerate(imgs_bytes,
                                                                 start=1):
-                    p = Path(uploaded_file.name)
+                    if isinstance(uploaded_file, int):
+                        p = Path(str(uploaded_file) + '.png')
+                    else:
+                        p = Path(uploaded_file.name)
+
                     img = remove_bg(bytes_data, p)
                     with io.BytesIO() as f:
                         img.save(f, format='PNG', quality=100, subsampling=0)
                         data = f.getvalue()
                     nobg_imgs.append((img, p, data))
 
-                    cur_progress = int(100 / len(uploaded_files))
+                    cur_progress = int(100 / len(imgs_bytes))
                     pb.progress(cur_progress * n)
                 time.sleep(1)
                 progress_bar.empty()
                 pb.success('Complete!')
 
-                col2.image([x[0] for x in nobg_imgs])
+                nobg_images = [x[0] for x in nobg_imgs]
+
+                if IS_GIF:
+                    nobg_gif = frames2gif(nobg_imgs, duration=duration)
+                    col2.image(nobg_gif)
+                    col2.markdown(
+                        '🧪 *Use [ezgif.com](https://ezgif.com/) to edit '
+                        'individual frames.*')
+                else:
+                    col2.image(nobg_images)
 
             if len(nobg_imgs) > 1:
                 with io.BytesIO() as tmp_zip:
@@ -111,8 +182,13 @@ def main():
                                         compress_type=zipfile.ZIP_DEFLATED)
                     zip_data = tmp_zip.getvalue()
 
+                if IS_GIF:
+                    frames_literal = '(individual frames)'
+                else:
+                    frames_literal = ''
+
                 down_btn.download_button(
-                    label='Download all results',
+                    label=f'Download all results {frames_literal}',
                     data=zip_data,
                     file_name=f'results_{int(time.time())}.zip',
                     mime='application/zip',
@@ -143,6 +219,8 @@ if __name__ == '__main__':
     logger.add('logs.log')
 
     load_dotenv()
+
+    ALLOW_GIF = True
 
     if len(sys.argv) > 1:
         MAX_FILES = int(sys.argv[1])
