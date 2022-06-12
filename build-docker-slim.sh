443104e5d2c1511f72322f48274a734187288b8e
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-06-12 16:41:28 -0400

    Update build-docker-slim.sh

--- build-docker-slim.sh
+++ build-docker-slim.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+export DOCKER_BUILDKIT=1
+
+docker build . --tag rembg-app
+
 echo | docker-slim build \
 	--http-probe=false \
 	--preserve-path=/usr/local/lib/python3.9 \
 	--preserve-path=/.u2net \
-	--tag=rembg-app-slim \
 	rembg-app
